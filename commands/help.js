fa2b4b8d457195a9b6d311fedd1e8f2d7c66198a
YOomDev <ype.oomes@gmail.com>
2024-09-21 17:11:31 +0200

    Fancy help command

---
+++ commands/help.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('Shows a list of all the bot commands and their descriptions.'),
+    async execute(interaction) {
+        const fields = [];
+        for (const command in interaction.client.commands) { fields.push(interaction.client.utils.createField(command.data.name, command.data.description)); }
+        await interaction.reply({ ephemeral: true, embeds: [interaction.client.utils.buildEmbed("Commands", "A list of all the possible commands for this bot:", fields)] });
+    },
+};

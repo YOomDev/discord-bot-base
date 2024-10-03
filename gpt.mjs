5618a8159c99800a35594d9d6db4f0cf63b8cc0f
YOomDev <ype.oomes@gmail.com>
2024-10-03 18:51:16 +0200

    Add error boolean to the result

--- gpt.mjs
+++ gpt.mjs
@@ -12,5 +12,5 @@ async function generateResponse(messages = [{ role: ROLES.USER, content: 'Why is
     return await ollama.chat({
         model: MODEL,
         messages: messages,
-    }).then(result => { return result }).catch(err => { console.error(err); return { message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
+    }).then(result => { result.error = false; return result }).catch(err => { console.error(err); return { error: true, message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
 }

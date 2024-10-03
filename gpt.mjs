11c107203bede8bd967dc6ac29c31e2fde0d277e
YOomDev <ype.oomes@gmail.com>
2024-10-03 20:50:49 +0200

    Fixed export

--- gpt.mjs
+++ gpt.mjs
@@ -1,16 +1,18 @@
 import ollama from 'ollama'
 
-const MODEL = 'llama3.2';
+module.exports = {
+    MODEL: 'llama3.2',
 
-const ROLES = {
-    SYSTEM: 'system',
-    USER:   'user',
-    GPT:    'assistant'
-}
+    ROLES: {
+        SYSTEM: 'system',
+        USER:   'user',
+        GPT:    'assistant'
+    },
 
-async function generateResponse(messages = [{ role: ROLES.USER, content: 'Why is the sky blue?' }]) {
-    return await ollama.chat({
-        model: MODEL,
-        messages: messages,
-    }).then(result => { result.error = false; return result }).catch(err => { console.error(err); return { error: true, message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
+    async generateResponse(messages = [{ role: ROLES.USER, content: 'Why is the sky blue?' }]) {
+        return await ollama.chat({
+            model: MODEL,
+            messages: messages,
+        }).then(result => { result.error = false; return result }).catch(err => { console.error(err); return { error: true, message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
+    }
 }

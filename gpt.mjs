42d423c30aaca34b8ac673eea2e7f52be019df6c
YOomDev <ype.oomes@gmail.com>
2024-10-03 17:37:24 +0200

    Made response generator use the given messages and added a small enum for the roles for message storing

--- gpt.mjs
+++ gpt.mjs
@@ -1,10 +1,16 @@
 import ollama from 'ollama'
 
-console.log(await generateResponse())
+const MODEL = 'llama3.2';
 
-async function generateResponse(messages = []) {
+const ROLES = {
+    SYSTEM: 'system',
+    USER:   'user',
+    GPT:    'assistant'
+}
+
+async function generateResponse(messages = [{ role: ROLES.USER, content: 'Why is the sky blue?' }]) {
     return await ollama.chat({
-        model: 'llama3.2',
-        messages: [{ role: 'user', content: 'Why is the sky blue?' }],
-    }).then(result => { return result.message.content }).catch(err => { console.error(err); return "Something went wrong with generating the response. an error log has been placed in the console of the bot." })
+        model: MODEL,
+        messages: messages,
+    }).then(result => { return result }).catch(err => { console.error(err); return { message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
 }

3db60deaea6c2e26c94da843f4a1c97c846f897d
YOomDev <ype.oomes@gmail.com>
2024-11-19 19:35:16 +0100

    Fix GPT reply failing due to discord interaction timeout and failed GPT response parsing

--- gpt.mjs
+++ gpt.mjs
@@ -1,4 +1,5 @@
 import ollama from 'ollama'
+import {logData} from "./utils.mjs";
 
 export const MODEL = 'llama3.2';
 export const ROLES = {
@@ -11,5 +12,5 @@ export async function generateResponse(messages = [{ role: ROLES.USER, content: 
     return await ollama.chat({
         model: MODEL,
         messages: messages,
-    }).then(result => { result.error = false; return result }).catch(err => { console.error(err); return { error: true, message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
+    }).then(result => { logData(result.message); return { error: false, message: { content: "" + result.message.content }} }).catch(err => { console.error(err); return { error: true, message: { content: "Something went wrong with generating the response. an error log has been placed in the console of the bot." } } })
 }
